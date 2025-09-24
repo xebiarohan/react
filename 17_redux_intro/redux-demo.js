fe622172ac656e900461e796cbb91829f4024ed9
Rohan Aggarwal <aggarwal.rohan17@gmail.com>
2025-09-24 21:05:08 +0400

    Redux in react intro

--- 17_redux_intro/redux-demo.js
+++ 17_redux_intro/redux-demo.js
@@ -1,9 +1,13 @@
 const redux = require("redux");
 
 const counterReducer = (state = { counter: 0 }, action) => {
-  return {
-    counter: state.counter + 1,
-  };
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  return state;
 };
 
 const store = redux.createStore(counterReducer);
@@ -15,5 +19,4 @@ const counterSubsriber = () => {
 
 store.subscribe(counterSubsriber);
 
-
-store.dispatch({type: 'increment'});
+store.dispatch({ type: "increment" });
