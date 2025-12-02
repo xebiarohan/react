44fc7b66686ac3c5e59b9dd36c9ce1c82a850e21
Rohan Aggarwal <aggarwal.rohan17@gmail.com>
2025-12-02 12:22:10 +0400

    React query continue

--- 21_tanstackQuery/src/components/Events/EditEvent.jsx
+++ 21_tanstackQuery/src/components/Events/EditEvent.jsx
@@ -1,9 +1,9 @@
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation } from "@tanstack/react-query";
 
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
-import { fetchEvent } from "../../util/http.js";
+import { fetchEvent, updateEvent, queryClient } from "../../util/http.js";
 import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
@@ -16,7 +16,28 @@ export default function EditEvent() {
     queryFn: ({ signal }) => fetchEvent({ id: params.id, signal }),
   });
 
-  function handleSubmit(formData) {}
+  const { mutate } = useMutation({
+    mutationFn: updateEvent,
+    onMutate: async (data) => {
+      const newEvent = data.event;
+      await queryClient.cancelQueries({queryKey: ["events", params.id]});
+      const previousData = queryClient.getQueryData(["events", params.id]);
+      queryClient.setQueryData(["events", params.id], newEvent);
+
+      return {previousData};
+    },
+    onError: (error, data, context) => {
+      queryClient.setQueryData(["events", params.id], context.previousData);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(['events', params.id]);
+    }
+  });
+
+  function handleSubmit(formData) {
+    mutate({id: params.id, event: formData});
+    navigate("../");
+  }
 
   function handleClose() {
     navigate("../");
@@ -61,7 +82,5 @@ export default function EditEvent() {
     );
   }
 
-  return <Modal onClose={handleClose}>
-    {content}
-  </Modal>;
+  return <Modal onClose={handleClose}>{content}</Modal>;
 }
--- 21_tanstackQuery/src/util/http.js
+++ 21_tanstackQuery/src/util/http.js
@@ -1,3 +1,4 @@
+
 import { QueryClient } from '@tanstack/react-query';
 
 export const queryClient = new QueryClient();
@@ -84,6 +85,25 @@ export async function deleteEvent({ id }) {
 
   if (!response.ok) {
     const error = new Error('An error occurred while deleting the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  return response.json();
+}
+
+export async function updateEvent({ id, event }) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({ event }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while updating the event');
     error.code = response.status;
     error.info = await response.json();
     throw error;
