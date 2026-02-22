e1d6b7dccf7cb1821430827e7942e890fe10b447
Rohan Aggarwal <aggarwal.rohan17@gmail.com>
2026-02-22 06:54:35 +0400

    Adding server components

--- 23_foodies-starting-project/app/meals/[slug]/page.js
+++ 23_foodies-starting-project/app/meals/[slug]/page.js
@@ -1,8 +1,39 @@
-export default function MealDetails({params}) {
-    return(
-        <>
-            <h1>Meal details page</h1>
-            <h2>{params.slug}</h2>
-        </>
-    )
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
+
+export default function MealDetails({ params }) {
+  const meal = getMeal(params.slug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill alt={meal.title} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
